0323b470a450083768ebb2da73c64cbfd47455c0
stanislaus laia <laiastanislaus170@gmail.com>
2025-11-02 00:43:36 +0700

    update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/prestasi", prestasiRoutes);
 
 // Root endpoint
 app.get("/", (req, res) => {
-  res.send("Server Prestasi berjalan stanislau!");
+  res.send("Server Prestasi berjalan stanislaus!");
 });
 
 // Jalankan server
