f778059288f6b6f81b34e5217e06640f3db30aa2
stanislaus laia <laiastanislaus170@gmail.com>
2025-11-08 15:30:44 +0700

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/prestasi", prestasiRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Server Prestasi berjalan stanislaus!");
+  res.send("Server Prestasi Berjalan!");
 });
 
 app.listen(PORT, () => {
